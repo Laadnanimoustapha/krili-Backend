342144d26fccfabd61ad422b4a8fa4f84379beb0
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2025-10-27 23:49:16 +0100

    jnn

--- app/sitemap-index.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next"
-import { getBaseUrl } from "@/lib/site"
-import { getBaseUrl } from "@/lib/site"
-import { getBaseUrl } from "@/lib/site"
-
-export default function sitemapIndex(): MetadataRoute.Sitemap {
-  const baseUrl = getBaseUrlconst baseUrl = getBaseUrl()
-  rturbaseUrl}/sitemap.xml`, lastModified: new Date() }]
-}
